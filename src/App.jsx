17090f0ce1ee4635c569ef24551636a2da3e6291
MohitTater815 <mohittater815@gmail.com>
2025-02-09 17:58:12 +0530

    add dasiy UI

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className="btn glass bg-slate-50">Glass button</button>
+    </>
+  )
+}
+
+export default App
